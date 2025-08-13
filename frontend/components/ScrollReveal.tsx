f9b686d2db4369f90dc7080ddcdd137985dd3747
Andrey-Tsirkun <andrey.tsirkun@gmail.com>
2025-08-13 19:35:42 +0300

    Add components directory to docker-compose, enhance next.config.js with webpack alias, and update TypeScript paths for components, app, styles, utils, types, and services.

---
+++ frontend/components/ScrollReveal.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const ScrollReveal: React.FC = () => {
+  return (
+    <div className="relative">
+      <div className="fixed inset-0">
+        <div className="absolute inset-0 flex items-center justify-center gap-16 bg-[#f2f2f2]">
+          <div className="flex gap-3 items-center">
+            <h1 className="text-6xl font-bold text-gray-900 flex items-end">
+              Exclamation<span className="text-red-500">!</span> <br />Art
+            </h1>
+            <span className="w-4 h-65 bg-gray-900" />
+            <h1 className="text-6xl font-bold text-gray-900">Studio</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ScrollReveal;
