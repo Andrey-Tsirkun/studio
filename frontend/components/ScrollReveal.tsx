34425ff7ad164161113c28067cfd182e094a67af
Andrey-Tsirkun <andrey.tsirkun@gmail.com>
2025-08-14 20:28:28 +0300

    Add ScrollReveal component with GSAP animations for scroll-triggered effects and update text styles in the frontend.

--- frontend/components/ScrollReveal.tsx
+++ frontend/components/ScrollReveal.tsx
@@ -1,16 +1,57 @@
+'use client';
+
 import React from 'react';
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 const ScrollReveal = () => {
+  gsap.registerPlugin(ScrollTrigger);
+  useGSAP(() => {
+    gsap.fromTo(
+      '.dark',
+      {
+        clipPath: 'inset(0% 100% 0% 0%)',
+      },
+      {
+        clipPath: 'inset(0% 0% 0% 0%)',
+        ease: 'none',
+        scrollTrigger: {
+          trigger: '.dark',
+          start: 'top top',
+          end: 'bottom top',
+          scrub: true,
+        },
+      }
+    );
+  });
+
   return (
     <div className="relative">
+      <div className="h-[200vh]" />
+
       <div className="fixed inset-0">
         <div className="absolute inset-0 flex items-center justify-center gap-16 bg-[#f2f2f2]">
           <div className="flex gap-3 items-center">
-            <h1 className="text-6xl font-bold text-gray-900 flex items-end">
-              Exclamation<span className="text-red-500">!</span> <br />Art
+            <h1 className="text-6xl font-bold text-gray-900">
+              Exclamation<span className="text-yellow-500">!</span> <br /> Art
             </h1>
-            <span className="w-4 h-65 bg-gray-900" />
-            <h1 className="text-6xl font-bold text-gray-900">Studio</h1>
+            <span className="w-3 h-56 bg-gray-900" />
+            <h1 className="text-6xl self-end font-bold text-gray-900">
+              Studio
+            </h1>
+          </div>
+        </div>
+
+        <div className="dark absolute inset-0 flex items-center justify-center gap-16 bg-gray-900">
+          <div className="flex gap-3 items-center">
+            <h1 className="text-6xl font-bold text-[#f2f2f2]">
+              Exclamation<span className="text-yellow-500">!</span> <br /> Art
+            </h1>
+            <span className="w-3 h-56 bg-[#f2f2f2]" />
+            <h1 className="text-6xl self-end font-bold text-[#f2f2f2]">
+              Studio
+            </h1>
           </div>
         </div>
       </div>
